5537edcb442515a3543ebc9ee167a87ed1e6a4aa
JeffCT0216 <30666694+JeffCT0216@users.noreply.github.com>
2018-07-07 17:01:29 -0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -13,3 +13,25 @@ const binarySearch = (arr, val) => {
   }
   return -1;
 }
+
+function bsRec(arr, n, begin, end) {
+begin = begin === undefined ? 0 : begin;
+end = end === undefined ? arr.length -  1 : end;
+if(arr.length < 1 || begin > end) return -1;
+let mid = begin + Math.floor((end - begin)/ 2);
+if(arr[mid] === n) return mid;
+if(arr[mid] > n) return bsRec(arr, n, begin, mid - 1);
+if(arr[mid] < n) return bsRec(arr, n, mid + 1, end);
+}
+
+function bsIte(arr, n) {
+  let begin = 0;
+  let end = arr.length - 1;
+  while(begin <= end) {
+    let mid = begin + Math.floor((end - begin) / 2);
+    if (arr[mid] === n) return mid;
+    if (arr[mid] > n) end = mid - 1;
+    if (arr[mid] < n) begin = mid + 1;
+  }
+  return - 1;
+}
